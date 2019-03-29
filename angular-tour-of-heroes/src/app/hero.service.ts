4f1fa01332e1882ad67580b458c0e9d4d676856b
Herkles <seanpatrickzeitler@gmail.com>
2019-03-29 17:31:00 -0400

    format for hero.service updated

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -5,11 +5,11 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
-
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
+
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
