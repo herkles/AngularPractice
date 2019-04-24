be7d67cd3c20962e671ceb879a6f14f2c64a1743
Herkles <seanpatrickzeitler@gmail.com>
2019-04-24 17:56:00 -0400

    created customers-list directory and files

--- PracticeApp/src/app/customers/customers.module.ts
+++ PracticeApp/src/app/customers/customers.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CustomersComponent }  from './customers.component';
+import { CustomersListComponent } from './customers-list/customers-list.component';
 
 @NgModule({
   imports:      [ CommonModule ],
-  declarations: [ CustomersComponent ],
+  declarations: [ CustomersComponent, CustomersListComponent ],
   exports: [ CustomersComponent ]
 })
 export class CustomersModule { }
