df53914dc36c0939749ddc96a12238af5b153acb
Herkles <seanpatrickzeitler@gmail.com>
2019-04-24 15:03:52 -0400

    created the customers directory

--- PracticeApp/src/app/app.component.ts
+++ PracticeApp/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{ title }}</h1>
+  `
 })
-export class AppComponent {
-  title = 'PracticeApp';
+
+export class AppComponent implements OnInit {
+  title: string;
+  constructor() { }
+
+  ngOnInit() {
+    this.title = 'Hello World'
+  }
+
 }
+
