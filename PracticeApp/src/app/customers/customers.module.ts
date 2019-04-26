51086c5d9fd056eb0808f4542fd36c6a6078b6de
Herkles <seanpatrickzeitler@gmail.com>
2019-04-26 15:42:49 -0400

    added filter textbox

--- PracticeApp/src/app/customers/customers.module.ts
+++ PracticeApp/src/app/customers/customers.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { CustomersComponent }  from './customers.component';
 import { CustomersListComponent } from './customers-list/customers-list.component';
+import { FilterTextboxComponent } from './customers-list/filter-textbox.component';
 
 @NgModule({
   imports:      [ CommonModule ],
-  declarations: [ CustomersComponent, CustomersListComponent ],
+  declarations: [ CustomersComponent, CustomersListComponent, FilterTextboxComponent ],
   exports: [ CustomersComponent ]
 })
 export class CustomersModule { }
