e17644c23872cc344e4b59869943ab9e4d3287c7
Herkles <seanpatrickzeitler@gmail.com>
2019-04-26 18:37:52 -0400

    added shared folder and interfaces

--- PracticeApp/src/app/app.module.ts
+++ PracticeApp/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { CustomersModule } from './customers/customers.module';
+import { SharedModule } from  './shared/shared.module';
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, CustomersModule ],
+  imports:      [ BrowserModule, CustomersModule, SharedModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
