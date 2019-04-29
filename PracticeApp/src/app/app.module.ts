999f9f3ff6c5bcdd03e8f1267de347df32e8e8f4
Herkles <seanpatrickzeitler@gmail.com>
2019-04-29 16:52:00 -0400

    creating custom pipes for data binding

--- PracticeApp/src/app/app.module.ts
+++ PracticeApp/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { SharedModule } from './shared/shared.module';
 import { CustomersModule } from './customers/customers.module';
-import { SharedModule } from  './shared/shared.module';
 import { AppComponent }  from './app.component';
 
 @NgModule({
--- PracticeApp/src/app/customers/customers.module.ts
+++ PracticeApp/src/app/customers/customers.module.ts
@@ -1,12 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
 import { CustomersComponent }  from './customers.component';
 import { CustomersListComponent } from './customers-list/customers-list.component';
 import { FilterTextboxComponent } from './customers-list/filter-textbox.component';
 
 @NgModule({
-  imports:      [ CommonModule ],
+  imports:      [ CommonModule, SharedModule ],
   declarations: [ CustomersComponent, CustomersListComponent, FilterTextboxComponent ],
   exports: [ CustomersComponent ]
 })
