f64807de541eb4c291dae71730189f3ba43b94b6
Herkles <seanpatrickzeitler@gmail.com>
2019-04-29 16:27:54 -0400

    more work with data binding

--- PracticeApp/src/app/customers/customers-list/customers-list.component.ts
+++ PracticeApp/src/app/customers/customers-list/customers-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { ICustomer } from '../../shared/interfaces';
 
@@ -7,6 +7,17 @@ import { ICustomer } from '../../shared/interfaces';
   templateUrl: './customers-list.component.html'
 })
 export class CustomersListComponent implements OnInit {
+  private _customers: ICustomer[] = [];
+  @Input() get customers(): ICustomer[] {
+    return this._customers;
+  }
+
+  set customers(value: ICustomer[]) {
+    if (value) {
+      this.filteredCustomers = this._customers = value;
+      this.calculateOrders();
+    }
+  }
 
   filteredCustomers: any[] = [];
   customersOrderTotal: number;
@@ -26,7 +37,6 @@ export class CustomersListComponent implements OnInit {
   }
 
   sort(prop: string) {
-    // A sorting service will handle the sorting
+    // A sorter service will handle the sorting
   }
-
 }
