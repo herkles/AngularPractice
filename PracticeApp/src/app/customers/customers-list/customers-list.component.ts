d139000b759287406d1575ae1d757fcad9fa23ff
Herkles <seanpatrickzeitler@gmail.com>
2019-04-29 16:15:28 -0400

    added data binding

--- PracticeApp/src/app/customers/customers-list/customers-list.component.ts
+++ PracticeApp/src/app/customers/customers-list/customers-list.component.ts
@@ -3,18 +3,30 @@ import { Component, OnInit } from '@angular/core';
 import { ICustomer } from '../../shared/interfaces';
 
 @Component({
-    selector: 'app-customers-list',
-    templateUrl: './customers-list.component.html'
+  selector: 'app-customers-list',
+  templateUrl: './customers-list.component.html'
 })
 export class CustomersListComponent implements OnInit {
 
-    filteredCustomers: ICustomer[] = [];
-    customersOrderTotal: number;
-    currencyCode: string = 'USD';
+  filteredCustomers: any[] = [];
+  customersOrderTotal: number;
+  currencyCode: string = 'USD';
 
-    constructor() {}
+  constructor() { }
 
-    ngOnInit() {
+  ngOnInit() {
 
-    }
+  }
+
+  calculateOrders() {
+    this.customersOrderTotal = 0;
+    this.filteredCustomers.forEach((cust: ICustomer) => {
+      this.customersOrderTotal += cust.orderTotal;
+    });
+  }
+
+  sort(prop: string) {
+    // A sorting service will handle the sorting
+  }
+
 }
