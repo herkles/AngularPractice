414348abd667dcf658b79747b477fa7bde9e26ee
Herkles <seanpatrickzeitler@gmail.com>
2019-04-30 14:44:16 -0400

    Calling the server wtih HttpClient

--- PracticeApp/src/app/core/data.service.ts
+++ PracticeApp/src/app/core/data.service.ts
@@ -11,10 +11,36 @@ export class DataService {
 
     baseUrl: string = 'assets/';
 
-    constructor() { }
+    constructor(private http: HttpClient) { }
 
+    getCustomers() : Observable<ICustomer[]> {
+        return this.http.get<ICustomer[]>(this.baseUrl + 'customers.json')
+            .pipe(
+                catchError(this.handleError)
+            );
+    }
 
+    getCustomer(id: number) : Observable<ICustomer> {
+      return this.http.get<ICustomer[]>(this.baseUrl + 'customers.json')
+        .pipe(
+          map(customers => {
+            let customer = customers.filter((cust: ICustomer) => cust.id === id);
+            return (customer && customer.length) ? customer[0] : null;
+          }),
+          catchError(this.handleError)
+        )
+    }
 
+    getOrders(id: number) : Observable<IOrder[]> {
+      return this.http.get<IOrder[]>(this.baseUrl + 'orders.json')
+        .pipe(
+          map(orders => {
+            let custOrders = orders.filter((order: IOrder) => order.customerId === id);
+            return custOrders;
+          }),
+          catchError(this.handleError)
+        );
+    }
 
     private handleError(error: any) {
       console.error('server error:', error);
@@ -26,5 +52,4 @@ export class DataService {
       }
       return Observable.throw(error || 'Node.js server error');
     }
-
 }
