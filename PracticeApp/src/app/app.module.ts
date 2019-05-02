2cf773f61bc1328ed6dce851d62fe40f12c16020
Herkles <seanpatrickzeitler@gmail.com>
2019-05-02 15:52:21 -0400

    adding a customers routing module and routes

--- PracticeApp/src/app/app.module.ts
+++ PracticeApp/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { CustomersModule } from './customers/customers.module';
 import { AppComponent }  from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule, CustomersModule, SharedModule, CoreModule ],
+  imports:      [ BrowserModule, CustomersModule, SharedModule, CoreModule, AppRoutingModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
--- PracticeApp/src/app/customers/customers.module.ts
+++ PracticeApp/src/app/customers/customers.module.ts
@@ -6,9 +6,10 @@ import { SharedModule } from '../shared/shared.module';
 import { CustomersComponent }  from './customers.component';
 import { CustomersListComponent } from './customers-list/customers-list.component';
 import { FilterTextboxComponent } from './customers-list/filter-textbox.component';
+import { CustomersRoutingModule } from './customers-routing.module';
 
 @NgModule({
-  imports:      [ CommonModule, SharedModule, FormsModule ],
+  imports:      [ CommonModule, SharedModule, FormsModule, CustomersRoutingModule ],
   declarations: [ CustomersComponent, CustomersListComponent, FilterTextboxComponent ],
   exports: [ CustomersComponent ]
 })
