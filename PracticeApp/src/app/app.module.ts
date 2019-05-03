3ce40dac385104f5d02b1964ed6da8bf474eaeb7
Herkles <seanpatrickzeitler@gmail.com>
2019-05-03 15:48:22 -0400

    PracticeApp is now finished

--- PracticeApp/src/app/app.module.ts
+++ PracticeApp/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { SharedModule } from './shared/shared.module';
 import { CustomersModule } from './customers/customers.module';
 import { AppComponent }  from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { OrdersModule } from './orders/orders.module';
 
 @NgModule({
-  imports:      [ BrowserModule, CustomersModule, SharedModule, CoreModule, AppRoutingModule ],
+  imports:      [ BrowserModule, CustomersModule, OrdersModule, SharedModule, CoreModule, AppRoutingModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
